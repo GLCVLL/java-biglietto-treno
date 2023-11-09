ceec511ef0085b6c53ad85b10c240c3dd89aba38
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-09 17:32:43 +0100

    added price with regular and % off

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -14,5 +14,22 @@ public class CalcolaBiglietto {
 
         System.out.print("Inserisci l'età del passeggero: ");
         int età = scanner.nextInt();
+        
+        // Calcolo il prezzo del biglietto
+        double prezzoBase = km * 0.21;
+        double prezzoFinale;
+
+        if (età < 18) {
+            // Sconto del 20% per i minorenni
+            prezzoFinale = prezzoBase * 0.8;
+        } else if (età >= 65) {
+            // Sconto del 40% per gli over 65
+            prezzoFinale = prezzoBase * 0.6;
+        } else {
+            prezzoFinale = prezzoBase;
+        }
+
+        System.out.println("Il prezzo del biglietto è: " + prezzoFinale + "€");
+
 	}
 }
